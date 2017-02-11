eafeb03ebd1abe687a260c4718446f2fd5d47ba1
kristenkeough <kristenkeough@kristens-mbp.attlocal.net>
2017-02-10 16:53:53 -0800

    Kristen individual assignment week 6

--- TrailOfZombies/src/control/InventoryControl.java
+++ TrailOfZombies/src/control/InventoryControl.java
@@ -24,5 +24,23 @@ public class InventoryControl {
 	double volume = (height * width * depth)/1728;
 
 	return volume;
-    }               
+    }  
+    
+    //Kristen Keough Individual Assignment
+    public double calcTireFootprint (double height, double width){
+    
+        if (width<0) { //width is negative?
+                return -1;
+    }
+        if (height<0 || height>30) { //width is out of range
+                return -1;
+    }
+        double radius = height / 2;
+        double volume = (2 * Math.PI * radius);
+        double footprint = (volume * width) / 144;
+        
+        return footprint;
+    }
 }
+
+
--- TrailOfZombies/test/control/InventoryControlTest.java
+++ TrailOfZombies/test/control/InventoryControlTest.java
@@ -150,7 +150,142 @@ public class InventoryControlTest {
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
     }
-    
+    //Kristen Keough Individual Assignment
+    /**
+     * Test of calcVolumeOfCrate method, of class InventoryControl.
+     */
+    @Test
+    public void calcTireFootprint() {
+        System.out.println("calcTireFootprint");
+        
+        /********
+         * Test Case 1
+         ********/
+        System.out.println("\tTest Case #1");
+        //input values for test case 1
+        double height = 30.0;
+        double width = 8.0;
+        
+        double expResult = 5.24; // expected output returned value
+        
+        //create instance of InventoryControl class
+        InventoryControl instance = new InventoryControl();
+        
+        //call function to run test
+        double result = instance.calcTireFootprint(height, width);
+        
+        //compare expected return value with actual value returned
+        assertEquals(expResult, result, 0.01);
+        // TODO review the generated test code and remove the default call to fail.
+        // fail("The test case is a prototype.");
+        
+        
+        /********
+         * Test Case 2
+         ********/
+        System.out.println("\tTest Case #2");
+        //input values for test case 1
+        height = -1.0;
+        width = 8.0;
+        
+        expResult = -1.0; // expected output returned value
+        
+        //create instance of InventoryControl class
+        instance = new InventoryControl();
+        
+        //call function to run test
+        result = instance.calcTireFootprint(height, width);
+        
+        //compare expected return value with actual value returned
+        assertEquals(expResult, result, 0.01);
+        // TODO review the generated test code and remove the default call to fail.
+        // fail("The test case is a prototype.");
+        
+        
+        /********
+         * Test Case 3
+         ********/
+        System.out.println("\tTest Case #3");
+        //input values for test case 1
+        height = 30.0;
+        width = -1.0;
+        
+        expResult = -1.0; // expected output returned value
+        
+        //create instance of InventoryControl class
+        instance = new InventoryControl();
+        
+        //call function to run test
+        result = instance.calcTireFootprint(height, width);
+        
+        //compare expected return value with actual value returned
+        assertEquals(expResult, result, 0.01);
+        // TODO review the generated test code and remove the default call to fail.
+        // fail("The test case is a prototype.");
+        
+        /********
+         * Test Case 4
+         ********/
+        System.out.println("\tTest Case #4");
+        //input values for test case 1
+        height = 31.0;
+        width = 8.0;
+        
+        expResult = -1.0; // expected output returned value
+        
+        //create instance of InventoryControl class
+        instance = new InventoryControl();
+        
+        //call function to run test
+        result = instance.calcTireFootprint(height, width);
+        
+        //compare expected return value with actual value returned
+        assertEquals(expResult, result, 0.01);
+        // TODO review the generated test code and remove the default call to fail.
+        // fail("The test case is a prototype.");
+        
+        /********
+         * Test Case 5
+         ********/
+        System.out.println("\tTest Case #5");
+        //input values for test case 1
+        height = 0.0;
+        width = 8.0;
+        
+        expResult = 0.0; // expected output returned value
+        
+        //create instance of InventoryControl class
+        instance = new InventoryControl();
+        
+        //call function to run test
+        result = instance.calcTireFootprint(height, width);
+        
+        //compare expected return value with actual value returned
+        assertEquals(expResult, result, 0.01);
+        // TODO review the generated test code and remove the default call to fail.
+        // fail("The test case is a prototype.");
+        
+        /********
+         * Test Case 6
+         ********/
+        System.out.println("\tTest Case #6");
+        //input values for test case 1
+        height = 30.0;
+        width = 0.0;
+        
+        expResult = 0.0; // expected output returned value
+        
+        //create instance of InventoryControl class
+        instance = new InventoryControl();
+        
+        //call function to run test
+        result = instance.calcTireFootprint(height, width);
+        
+        //compare expected return value with actual value returned
+        assertEquals(expResult, result, 0.01);
+        // TODO review the generated test code and remove the default call to fail.
+        // fail("The test case is a prototype.");
+    }
 }
     
     
